465276b9a6096149270c6a33c6f3edc1e502b5ec
Douglas Kagai <kagaidouglas@gmail.com>
2022-03-22 10:32:14 +0300

    add changes

---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+          id: Math.floor(Math.random() * 1000),
+            text: input
+        })
+    setInput('');
+    };
+  return (
+    <div>
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input type="text" placeholder='Add a todo' value={input} name="text"
+            className='todo-input' onChange={handleChange}/>
+            <button className='todo-button'>
+                Add todo
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React,  { useState } from 'react'
+import TodoForm from './TodoForm';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)){
+      return
+    }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+  return (
+    <div>
+      <h1>What's your Plan Today?</h1>
+      <TodoForm onSubmit={addTodo}/>
+    </div>
+  )
+}
+
+
+
+
+
+export default TodoList;
